f0a1b7a48698051ef442923e3b13e7d05626bfc3
SuperParadox <1521012642@qq.com>
2023-12-18 18:13:40 +0800

    汇总列表范围功能大致完成

--- test.py
+++ test.py
@@ -7,16 +7,16 @@
 import pandas as pd
 from nptyping import DataFrame
 # from pandas.api.types import CategoricalDtype
-from my_modules.occupational_health import OccupationalHealthItemInfo#, refresh_engaged_num
-from my_modules.other_infos import templates_info
+from occupational_health_module.occupational_health_backup import OccupationalHealthItemInfo#, refresh_engaged_num
+# from occupational_health_module.other_infos import templates_info
 
-company_name: str = 'MSCN'
-project_name: str = '23ZXP0000'
+company_name: str = '中石化森美(福建)石油有限公司宁德城南加油站'
+project_name: str = '23ZXP0026-3'
 
-file_path: str = r'./templates/项目信息试验模板.xlsx'
+file_path: str = r'./templates/项目信息试验模板t2.xlsx'
 point_info_df: DataFrame = pd.read_excel(file_path, sheet_name='定点') # type: ignore
 personnel_info_df: DataFrame = pd.read_excel(file_path, sheet_name='个体') # type: ignore
 
-new_project = OccupationalHealthItemInfo(company_name, project_name, point_info_df, personnel_info_df, templates_info)
+new_project = OccupationalHealthItemInfo(company_name, project_name, point_info_df, personnel_info_df)
 
 new_project.get_dfs_num(new_project.default_types_order)
