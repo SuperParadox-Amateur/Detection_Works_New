b2fc655ea87a1c9d859dd09e3f26ed91f4bc44e3
邱 智杭 <1521012642@qq.com>
2024-03-27 23:12:52 +0800

    继续更新

--- occupational_health_module/new_occupational_health.py
+++ occupational_health_module/new_occupational_health.py
@@ -5,12 +5,15 @@ import json
 import math
 from pathlib import Path
 from decimal import ROUND_HALF_DOWN, Decimal
+import re
 from typing import Any, Dict, List
 
 from nptyping import DataFrame
 import pandas as pd
 from pandas import CategoricalDtype
 from docx import Document
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.shared import Pt
 
 
 class NewOccupationalHealthItemInfo():
@@ -20,6 +23,7 @@ class NewOccupationalHealthItemInfo():
             project_number: str,
             company_name: str,
             raw_df: DataFrame[Any],
+            is_all_factors_split: bool = False,
             ) -> None:
         self.company_name: str = company_name  # 公司名称
         self.project_number: str = project_number  # 项目编号
@@ -29,6 +33,7 @@ class NewOccupationalHealthItemInfo():
         self.templates_info_dict: Dict[str, Any] = (
             self.read_json_to_dict('./info_files/默认模板格式信息.json')
         )
+        self.is_all_factors_split: bool = is_all_factors_split # 是否所有检测因素按照日期分开
         self.factor_reference_df: DataFrame[Any] = self.get_occupational_health_factor_reference()
         self.df: DataFrame[Any] = self.initialize_df(raw_df)
         self.schedule_col: str = self.initialize_schedule()
@@ -507,32 +512,106 @@ class NewOccupationalHealthItemInfo():
         # [ ] 有害物质要分日程和不同检测因素
         for day_idx, schedule in enumerate(self.schedule_list):
             # 定点有害物质
-            doc_point: Any = Document(self.templates_info_dict['有害物质定点']['template_path'])
-            point_info_dict: Dict[str, Any] = self.templates_info_dict['有害物质定点']
             current_point_df: DataFrame = (
                 self.point_df  # type:ignore
                 [self.point_df[self.schedule_col] == schedule]
                 .reset_index(drop=True)
             )
-            # [ ] 方法要设置成df是0就跳过
+            # 当天的定点有害物质数量是0就跳过
+            if current_point_df.shape[0] != 0:
+                doc_point: Any = Document(self.templates_info_dict['有害物质定点']['template_path'])
+                point_info_dict: Dict[str, Any] = self.templates_info_dict['有害物质定点']
+                factors: List[str] = current_point_df['检测参数'].drop_duplicates().tolist()
+                # 获得当前检测因素的dataframe
+                for current_factor in factors:
+                    # 导入模板
+                    doc_point_copy: Any = deepcopy(doc_point)
+                    current_factor_df: DataFrame = (
+                        current_point_df[current_point_df['检测参数'] == current_factor]
+                        .sort_values(by='测点编号')
+                        .reset_index(drop=True)
+                    )
+                    self.write_point_deleterious_substance(
+                        doc_point_copy,
+                        point_info_dict,
+                        current_factor_df,
+                        current_factor,
+                        day_idx,
+                        schedule
+                    )
+            # 个体有害物质
+            current_personnel_df: DataFrame = (
+                self.personnel_df  # type:ignore
+                [self.personnel_df[self.schedule_col] == schedule]
+                .reset_index(drop=True)
+            )
+            # 当天的个体有害物质数量是0就跳过
+            if current_personnel_df.shape[0] != 0:
+                doc_personnel: Any = Document(self.templates_info_dict['有害物质个体']['template_path'])
+                personnel_info_dict: Dict[str, Any] = self.templates_info_dict['有害物质个体']
+                factors: List[str] = current_personnel_df['检测参数'].drop_duplicates().tolist()
+                # 获得当前检测因素的dataframe
+                for current_factor in factors:
+                    # 导入模板
+                    doc_personnel_copy: Any = deepcopy(doc_personnel)
+                    current_factor_df: DataFrame = (
+                        current_personnel_df[current_personnel_df['检测参数'] == current_factor]
+                        .sort_values(by='测点编号')
+                        .reset_index(drop=True)
+                    )
+                    self.write_personnel_deleterious_substance(
+                        doc_personnel_copy,
+                        personnel_info_dict,
+                        current_factor_df,
+                        current_factor,
+                        day_idx,
+                        schedule
+                    )
+            # [ ] 流转单
+        # [ ] 直读检测因素设置为是否按照日程分开
+            # 个体噪声
+            # 获得个体噪声信息
+            # 个体噪声记录表是否按照日程分开
+            if self.is_all_factors_split:
+                query_str: str = (
+                    '采样方式 == "个体"'
+                    ' and '
+                    '检测参数 == "噪声"'
+                    ' and '
+                    f'{self.schedule_col} == "{schedule}"'
+                )
+            else:
+                query_str: str = (
+                    '采样方式 == "个体"'
+                    ' and '
+                    '检测参数 == "噪声"'
+                )
+            current_factor_df: DataFrame = (
+                self.df
+                .query(query_str)
+                .sort_values('测点编号')
+                .reset_index(drop=True)
+            )
+            # 当天的个体噪声数量是0就跳过
+            if current_factor_df.shape[0] != 0:
+                doc_personnel_noise: Any = Document(self.templates_info_dict['噪声个体']['template_path'])
+                personnel_noise_info_dict: Dict[str, Any] = self.templates_info_dict['噪声个体']
 
-    def write_info_to_doc(
+    def write_point_deleterious_substance(
             self,
-            doc: Any,
+            doc_copy: Any,
             info_dict: Dict[str, Any],
-            current_df: DataFrame[Any],
+            current_factor_df: DataFrame[Any],
+            factor: str,
             day_idx: int,
             schedule: Any
         ) -> None:
         '''将相应信息写入对应模板'''
-        # [ ] 计划让所有写入模板的方法统一成一个方法
-        # 导入模板
-        doc_copy: Any = deepcopy(doc)
         # 计算需要的记录表页数
         table_pages: int = (
             math
             .ceil(
-                (len(current_df) - info_dict['first_page_rows'])
+                (len(current_factor_df) - info_dict['first_page_rows'])
                 / info_dict['late_page_rows'] + 1
             )
         )
@@ -557,3 +636,238 @@ class NewOccupationalHealthItemInfo():
                 new_paragraph = doc_copy.add_page_break()
                 new_paragraph._p.addnext(new_table._element)
                 doc_copy.add_paragraph()
+        # 确定不同的表格要填入的信息范围
+        tables = doc_copy.tables
+        for table_page in range(table_pages):
+            if table_page == 0:
+                index_first: int = 0
+                index_last: int = info_dict['first_page_rows'] - 1
+            else:
+                index_first: int = (
+                    info_dict['late_page_rows']
+                    * (table_page - 1)
+                    + info_dict['first_page_rows']
+                )
+                index_last: int = (
+                    info_dict['first_page_rows']
+                    + table_page
+                    * info_dict['late_page_rows']
+                    - 1
+                )
+            current_df: DataFrame = (
+                current_factor_df
+                .query(f'index >= {index_first} and index <= {index_last}')
+                .reset_index(drop=True)
+            )
+            # 向指定表格填写数据
+            current_table = tables[table_page + 1]
+            for r_i in range(current_df.shape[0]):
+                row_info = {
+                    '采样点编号': current_df.loc[r_i, '测点编号'],
+                    '采样岗位': f"{current_df.loc[r_i, '单元']}\n{current_df.loc[r_i, '检测地点']}",
+                    '空白编号1': current_df.loc[r_i, '空白编号1'],
+                    '空白编号2': current_df.loc[r_i, '空白编号2'],
+                    '样品编号': current_df.loc[r_i, '样品编号'],
+                    '代表时长': current_df.loc[r_i, '代表时长'],
+                    '是否合并代表时长': current_df.loc[r_i, '是否合并代表时长'],
+                }
+                # 采样点编号单元格
+                cell1 = current_table.cell(r_i * 6 + 2, 0)
+                cell1.text = str(row_info['采样点编号'])
+                cell1.paragraphs[0].paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER # type: ignore
+                cell1.paragraphs[0].runs[0].font.size = Pt(8)
+                # 采样岗位单元格
+                cell2 = current_table.cell(r_i * 6 + 2, 1)
+                cell2.text = row_info['采样岗位']
+                cell2.paragraphs[0].paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER # type: ignore
+                cell2.paragraphs[0].runs[0].font.size = Pt(7.5)
+                #[x] 样品编号加上项目编号前缀
+                # 空白编号单元格，只写入第一行
+                if table_page == 0 and r_i == 0 and row_info['空白编号1'] != '-':
+                    cell3_1 = current_table.cell(r_i * 6 + 2, 2)
+                    cell3_1.text = f"{self.project_number}{row_info['空白编号1']}"
+                    cell3_2 = current_table.cell(r_i * 6 + 3, 2)
+                    cell3_2.text = f"{self.project_number}{row_info['空白编号2']}"
+                    cell3_1.paragraphs[0].runs[0].font.size = Pt(8)
+                    cell3_2.paragraphs[0].runs[0].font.size = Pt(8)
+                else:
+                    pass
+                # 样品编号单元格
+                for n_i, num in enumerate(row_info['样品编号']):
+                    cell4 = current_table.cell(r_i * 6 + n_i + 4, 2)
+                    cell4.text = f"{self.project_number}{num:0>4d}"
+                    cell4.paragraphs[0].runs[0].font.size = Pt(8)
+                # 代表时长
+                for n_i, duration in enumerate(row_info['代表时长']):
+                    cell5 = current_table.cell(r_i * 6 + n_i + 4, 9)
+                    cell5.text = str(duration)
+                    cell5.paragraphs[0].paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER # type: ignore
+                    cell5.paragraphs[0].runs[0].font.size = Pt(9)
+                # 是否合并代表时长
+                if row_info['是否合并代表时长'] == True:
+                    merge_len: int = len(row_info['样品编号'])
+                    merge_cell1 = current_table.cell(r_i * 6 + 4, 9)
+                    merge_cell2 = current_table.cell(r_i * 6 + merge_len + 3, 9)
+                    merge_cell1.merge(merge_cell2)
+        #[x] 写入项目基本信息
+        info_table = tables[0]
+        # 项目编号
+        code_cell = info_table.cell(0, 1)
+        code_cell.text = self.project_number
+        # 单位
+        comp_cell = info_table.cell(0, 4)
+        comp_cell.text = self.company_name
+        # 检测因素
+        item_cell = info_table.cell(3, 1)
+        item_cell.text = str(factor)
+        # 采样日期
+        date_cell = info_table.cell(3, 6)
+        if self.schedule_col == '采样/送样日期':
+            date_cell.text = schedule.strftime("%Y年%m月%d日")
+        for cell in [code_cell, comp_cell, item_cell, date_cell]:
+            p = cell.paragraphs[0]
+            p.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER  # type: ignore
+            if len(cell.text) >= 14:
+                p.runs[0].font.size = Pt(8)
+            else:
+                pass
+                # p.runs[0].font.size = Pt(9)
+        # 页脚信息
+        core_properties = doc_copy.core_properties
+        core_properties.keywords = factor
+        if self.schedule_col == "采样/送样日期":
+            core_properties.comments  = schedule.strftime(r"%Y/%m/%d")
+        # 保存到桌面文件夹里
+        file_name: str = f'D{day_idx + 1}-定点-{factor}'
+        safe_file_name: str = re.sub(r'[?*/\<>:"|]', ',', file_name)
+        file_output_path: Path = self.output_path/safe_file_name
+        doc_copy.save(f'{file_output_path}.docx')
+
+    def write_personnel_deleterious_substance(
+            self,
+            doc_copy: Any,
+            info_dict: Dict[str, Any],
+            current_factor_df: DataFrame[Any],
+            factor: str,
+            day_idx: int,
+            schedule: Any
+    ):
+        '''将个体有害物质信息写入模板'''
+        # 计算需要的记录表页数
+        table_pages: int = (
+            math
+            .ceil(
+                (len(current_factor_df) - info_dict['first_page_rows'])
+                / info_dict['late_page_rows'] + 1
+            )
+        )
+
+        # 按照页数来增减表格数量
+        if table_pages == 1:
+            rm_table = doc_copy.tables[2]
+            t = rm_table._element
+            t.getparent().remove(t)
+            rm_page_break = doc_copy.paragraphs[-2]
+            pg = rm_page_break._element
+            pg.getparent().remove(pg)
+            rm_page_break2 = doc_copy.paragraphs[-2]
+            pg2 = rm_page_break2._element
+            pg2.getparent().remove(pg2)
+        elif table_pages == 2:
+            pass
+        else:
+            for _ in range(table_pages - 2):
+                cp_table = doc_copy.tables[2]
+                new_table = deepcopy(cp_table)
+                new_paragraph = doc_copy.add_page_break()
+                new_paragraph._p.addnext(new_table._element)
+                doc_copy.add_paragraph()
+        # 确定不同的表格要填入的信息范围
+        tables = doc_copy.tables
+        for table_page in range(table_pages):
+            if table_page == 0:
+                index_first: int = 0
+                index_last: int = info_dict['first_page_rows'] - 1
+            else:
+                index_first: int = (
+                    info_dict['late_page_rows']
+                    * (table_page - 1)
+                    + info_dict['first_page_rows']
+                )
+                index_last: int = (
+                    info_dict['first_page_rows']
+                    + table_page
+                    * info_dict['late_page_rows']
+                    - 1
+                )
+            current_df: DataFrame = (
+                current_factor_df
+                .query(f'index >= {index_first} and index <= {index_last}')
+                .reset_index(drop=True)
+            )
+            # 向指定表格填写数据
+            current_table = tables[table_page + 1]
+            for r_i in range(current_df.shape[0]):
+                row_info = {
+                    '采样点编号': current_df.loc[r_i, '测点编号'],
+                    '采样岗位': f"{current_df.loc[r_i, '单元']}\n{current_df.loc[r_i, '工种/岗位']}",
+                    '样品编号': current_df.loc[r_i, '样品编号'],
+                    '代表时长': current_df.loc[r_i, '日接触时长/h'],
+                }
+                # 采样点编号单元格
+                cell1 = current_table.cell(r_i * 3 + 2, 0)
+                cell1.text = str(row_info['采样点编号'])
+                cell1.paragraphs[0].paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER # type: ignore
+                cell1.paragraphs[0].runs[0].font.size = Pt(8)
+                # 采样岗位单元格
+                cell2 = current_table.cell(r_i * 3 + 2, 1)
+                cell2.text = row_info['采样岗位']
+                cell2.paragraphs[0].paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER # type: ignore
+                cell2.paragraphs[0].runs[0].font.size = Pt(7.5)
+                #[x] 样品编号加上项目编号前缀
+                # 样品编号单元格
+                cell4 = current_table.cell(r_i * 3 + 2, 2)
+                cell4.text = f"{self.project_number}{row_info['样品编号']:0>4d}"
+                cell4.paragraphs[0].runs[0].font.size = Pt(8)
+                # 代表时长
+                cell5 = current_table.cell(r_i * 3 + 2, 4)
+                cell5.text = str(row_info['代表时长'])
+                cell5.paragraphs[0].paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER # type: ignore
+                cell5.paragraphs[0].runs[0].font.size = Pt(9)
+        #[x] 写入项目基本信息
+        info_table = tables[0]
+        # 项目编号
+        code_cell = info_table.cell(0, 1)
+        code_cell.text = self.project_number
+        # 单位
+        comp_cell = info_table.cell(0, 4)
+        comp_cell.text = self.company_name
+        # 检测因素
+        item_cell = info_table.cell(3, 1)
+        item_cell.text = str(factor)
+        # 采样日期
+        date_cell = info_table.cell(3, 6)
+        if self.schedule_col == '采样/送样日期':
+            date_cell.text = schedule.strftime("%Y年%m月%d日")
+        for cell in [code_cell, comp_cell, item_cell, date_cell]:
+            p = cell.paragraphs[0]
+            p.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER  # type: ignore
+            if len(cell.text) >= 14:
+                p.runs[0].font.size = Pt(8)
+            else:
+                pass
+                # p.runs[0].font.size = Pt(9)
+        # 页脚信息
+        core_properties = doc_copy.core_properties
+        core_properties.keywords = factor
+        if self.schedule_col == "采样/送样日期":
+            core_properties.comments  = schedule.strftime(r"%Y/%m/%d")
+        # 保存到桌面文件夹里
+        file_name: str = f'D{day_idx + 1}-个体-{factor}'
+        safe_file_name: str = re.sub(r'[?*/\<>:"|]', ',', file_name)
+        file_output_path: Path = self.output_path/safe_file_name
+        doc_copy.save(f'{file_output_path}.docx')
+
+    def write_personnel_noise(
+            self,
+    )
